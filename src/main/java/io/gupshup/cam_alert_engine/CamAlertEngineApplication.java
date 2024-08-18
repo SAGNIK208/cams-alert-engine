d46eca50154804d921b6399c30dd7db87496a690
SAGNIK208 <sagnik.guru20@gmail.com>
2024-08-18 12:24:19 +0530

    initial commit

---
+++ src/main/java/io/gupshup/cam_alert_engine/CamAlertEngineApplication.java
@@ -0,0 +1,13 @@
+package io.gupshup.cam_alert_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamAlertEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamAlertEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gupshup/cam_alert_engine/CamAlertEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gupshup.cam_alert_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamAlertEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
