b5775c3ee4cd7eddd68b60c2deeba68d344a42d3
SAGNIK208 <sagnik.guru20@gmail.com>
2024-08-21 10:11:23 +0530

    changes for grafana

---
+++ src/main/java/io/gupshup/cam_alert_engine/utils/ObjectMapperConfig.java
@@ -0,0 +1,12 @@
+package io.gupshup.cam_alert_engine.utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
